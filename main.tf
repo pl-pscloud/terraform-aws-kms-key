f213ce819cdbad6ae3d8ee0f3104b1044089e170
pswitala <p.switala@pslab.eu>
2022-03-04 13:35:01 +0100

    Added key policy document

--- main.tf
+++ main.tf
@@ -3,6 +3,8 @@ resource "aws_kms_key" "pscloud-kms-key" {
   enable_key_rotation  = var.pscloud_enable_key_rotation
   multi_region         = var.pscloud_multi_region
 
+  policy               = var.pscloud_policy
+
   tags = {
     Name          = "${var.pscloud_company}_kms_key_${var.pscloud_env}"
     Project       = var.pscloud_project
--- vars.tf
+++ vars.tf
@@ -4,3 +4,5 @@ variable "pscloud_project" {}
 
   variable "pscloud_enable_key_rotation" { default = false }
   variable "pscloud_multi_region" { default = false }
+
+  variable "pscloud_policy" { default = "" }
